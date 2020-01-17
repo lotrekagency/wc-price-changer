0ac51f8fae42528888bea53a46a86343f8836056
Edoardo Mazzucchielli <mazzucchielli.edoardo@gmail.com>
2020-01-17 17:29:55 +0100

    Added query for WC products retrieving

--- wc-price-changer.php
+++ wc-price-changer.php
@@ -9,11 +9,11 @@
 add_action('admin_menu', 'setup_menu');
  
 function setup_menu(){
-    if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))){
         add_submenu_page(
             'woocommerce',
             'Price Changer',
-            'Price Changer',
+            'WC Price Changer',
             'manage_options',
             'price-changer',
             'setup_page'
@@ -23,5 +23,11 @@ function setup_menu(){
  
 function setup_page(){
     echo "<h1>WC Price Changer</h1>";
+    $products = wc_get_products(array('status' => 'publish'));
+
+    foreach ( $products as $product ){
+        echo 'ID: ' . $product->get_id() . '<br>';
+        echo 'Title: ' . $product->get_title() . '<br>';
+    }
 }
 ?>
